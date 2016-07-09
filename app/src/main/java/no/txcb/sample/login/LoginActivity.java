422b6aeee163627050ba901ccb2fb78a37a7cb61
Alexander Arnesen <alexander.arnesen@knowit.no>
2016-07-09 20:33:12 +0200

    Updated libraries.

--- app/src/main/java/no/txcb/sample/login/LoginActivity.java
+++ app/src/main/java/no/txcb/sample/login/LoginActivity.java
@@ -2,16 +2,9 @@ package no.txcb.sample.login;
 
 import android.content.Context;
 import android.databinding.DataBindingUtil;
-import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 import no.txcb.sample.R;
 import no.txcb.sample.databinding.ActivityLoginBinding;
 import no.txcb.sample.databinding.ErrorModel;
@@ -27,27 +20,9 @@ public class LoginActivity extends AppCompatActivity implements loginView {
 
     private CompositeSubscription compositeSubscription = new CompositeSubscription();
 
-    @Bind(R.id.welcome_text)
-    TextView welcomeText;
-
-    @Bind(R.id.username)
-    EditText userName;
-
-    @Bind(R.id.password)
-    EditText password;
-
-    @Bind(R.id.button)
-    Button loginButton;
-
-
     private ErrorModel errorModel;
+    private ActivityLoginBinding binding;
 
-
-    @OnClick(R.id.button)
-    void clickButton() {
-        loginPresenter.loginUser(userName.getText().toString(), password.getText().toString());
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,20 +33,20 @@ public class LoginActivity extends AppCompatActivity implements loginView {
         loginPresenter.attachView(this);
 
         // Databinding
-        ViewDataBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
         errorModel = new ErrorModel();
-        ((ActivityLoginBinding) binding).setErrorModel(errorModel);
-        ButterKnife.bind(this);
+        binding.setErrorModel(errorModel);
 
-
+        binding.loginContent.button.setOnClickListener(view ->
+                loginPresenter.loginUser(binding.loginContent.username.getText().toString(), binding.loginContent.password.getText().toString()));
         PublishSubject<String> usernameSubject = PublishSubject.create();
         PublishSubject<String> passwordSubject = PublishSubject.create();
 
-        RxAssist.setOnTextChanged(userName, usernameSubject);
-        RxAssist.setOnTextChanged(password, passwordSubject);
+        RxAssist.setOnTextChanged(binding.loginContent.username, usernameSubject);
+        RxAssist.setOnTextChanged(binding.loginContent.password, passwordSubject);
         Subscription subscribe = Observable.combineLatest(usernameSubject, passwordSubject,
                 (username, password) -> username.length() > 5 && password.length() > 5)
-                .subscribe(aBoolean -> loginButton.setEnabled(aBoolean));
+                .subscribe(binding.loginContent.button::setEnabled);
         compositeSubscription.add(subscribe);
     }
 
@@ -84,7 +59,7 @@ public class LoginActivity extends AppCompatActivity implements loginView {
 
     @Override
     public void setWelcomeText(String text) {
-        welcomeText.setText(text);
+        binding.loginContent.welcomeText.setText(text);
     }
 
     @Override
@@ -104,6 +79,6 @@ public class LoginActivity extends AppCompatActivity implements loginView {
 
     @Override
     public void setErrorText(String text) {
-        password.setError(text);
+        binding.loginContent.password.setError(text);
     }
 }
